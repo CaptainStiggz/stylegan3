11a791d15f2cf4f27c05b6907f2dd641e1b79e54
Z <zstiggelbout@gmail.com>
2022-01-13 16:32:37 -0400

    synth

--- synthesis/synthesis.py
+++ synthesis/synthesis.py
@@ -186,8 +186,7 @@ def create_kaleidoscope(imgs):
   print("out.shape", out.shape, out.dtype)
   return out
 
-def synthesize_kaleidoscope(G, device, num_seeds = 4, w_frames = 20):
-  seeds = [random.randint(1, 10000) for _ in range(num_seeds)]
+def synthesize_kaleidoscope(G, device, seeds, w_frames = 20):  
   print(seeds)
   imgs = synthesize_rand_interp(seeds, G, device, w_frames)
   return create_kaleidoscope(imgs)
